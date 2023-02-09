edb1b196d98e3b34d844db062601d5ce2165c220
Kento nakasone <e225748@ie.u-ryukyu.ac.jp>
2023-02-09 21:24:55 +0900

    STEP2

--- app/src/test/java/jp/ac/uryukyu/ie/e225748/WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225748/WarriorTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class WarriorTest {
     @Test void attackWithWeponSkillTest(){
         Warrior demo = new Warrior("Demo戦士", 100, 100);
-        int enemyDefaultHP = 1000;
-        Enemy slime = new Enemy("Demoスライム", enemyDefaultHP, 100);
+        int enemyHP = 1000;
+        Enemy slime = new Enemy("Demoスライム", enemyHP, 100);
         for(int i=0; i<3; i++){
             demo.attackWithWeponSkill(slime);
-            assertEquals(demo.attack * 3 /2, enemyDefaultHP - slime.hitPoint);
-            enemyDefaultHP = slime.hitPoint;
+            assertEquals(demo.attack * 3 /2, enemyHP - slime.hitPoint);
+            enemyHP = slime.hitPoint;
         }
     }
 }
